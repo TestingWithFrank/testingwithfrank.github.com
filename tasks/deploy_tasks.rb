eb2010b60f4a8b87cc0540e30ca9a89fd2de257a
Pete Hodgson <git@thepete.net>
2013-05-26 00:05:56 -0700

    only upload files which haven't changed

--- tasks/deploy_tasks.rb
+++ tasks/deploy_tasks.rb
@@ -1,13 +1,23 @@
 require_relative 'deployer'
 
+PREVIEW_AWS_BUCKET = "preview.testingwithfrank.com"
 PRODUCTION_AWS_BUCKET = "preview.testingwithfrank.com"
+
+def load_aws_creds
+  {
+    :access_key_id => ENV.fetch('AWS_ACCESS_KEY_ID'),
+    :secret_access_key => ENV.fetch('AWS_SECRET_ACCESS_KEY')
+  }
+end
+
+def deploy_to_bucket(bucket)
+  deployer = S3Deployer.new( load_aws_creds, bucket )
+  public_dir = File.expand_path("../../public",__FILE__)
+  deployer.upload_dir( public_dir )
+end
 
 desc "deploy to production"
 task :deploy do
-  aws_access_key_id = ENV.fetch('AWS_ACCESS_KEY_ID')
-  aws_secret_access_key = ENV.fetch('AWS_SECRET_ACCESS_KEY')
-
-  deployer = S3Deployer.new( aws_access_key_id, aws_secret_access_key )
-  deployer.upload_dir( File.expand_path("../../public",__FILE__), PRODUCTION_AWS_BUCKET )
+  deploy_to_bucket(PRODUCTION_AWS_BUCKET)
 end
 
